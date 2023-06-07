aa88688287aeb4f658c35adbc7629f4a6fb94f5a
hisham <hisham.nasralla@gmail.com>
2023-06-07 13:26:45 +0300

    modify grouping files to be patched grouping, it will take 100 files in each time, and use helper class and functions instead of old code to be more sortable, clean, and scalable.

--- main.py
+++ main.py
@@ -1,38 +1,59 @@
 #!/usr/bin/env python3
-
 import os
 import shutil
 
-# Define the directory where the text files are
-# located and in this case its in the app root folder
-current_directory = os.getcwd()
-files_directory = os.path.join(current_directory, "files")
 
-# create a list of all the text files in the directory
-files = os.listdir(files_directory)
+class CategorizeLangFilesHelper:
+    def __init__(self):
+        self.current_directory = None
+        self.files_directory = None
 
-# here to make sure only .txt file are detected
-files = [file for file in files if file.endswith(".txt")]
-
-# grouping the files by language
-groups = {}
-for file in files:
-    language = file.split("-")[0]
-    groups.setdefault(language, []).append(file)
-
-# create sub-folders if not exists for each language and move the files
-for language, files in groups.items():
-
-    # create sub-folder
-    os.makedirs(f'{files_directory}/{language}', exist_ok=True)
-
-    # getting sub-foldet path
-    subfolder = os.path.join(current_directory, files_directory, language)
-
-    # move the files to the sub-folders
-    for file in files:
-        source = os.path.join(files_directory, file)
-        destination = os.path.join(subfolder, file)
+    def move_file_to_specific_location(self, source, destination):
         shutil.move(source, destination)
 
-print("Files have been grouped into sub-folders based on language.")
+    def create_new_directory(self, sub_folder_name):
+        os.makedirs(f'{self.files_directory}/{sub_folder_name}', exist_ok=True)
+
+    def get_files_dir(self):
+        """
+        This function gets the files directory (in this case, it's in the app root folder)
+        """
+        self.current_directory = os.getcwd()
+        self.files_directory = os.path.join(self.current_directory, "files")
+
+    def grouping_files(self):
+        """
+        This function gets all files and groups them
+        """
+        files = os.listdir(self.files_directory)
+        files = [file for file in files if file.endswith(".txt")]
+        groups = {}
+        patch_size = 100
+
+        for i in range(0, len(files), patch_size):
+            patch_files = files[i:i + patch_size]
+            for file in patch_files:
+                language = file.split("-")[0]
+                groups.setdefault(language, []).append(file)
+
+            # Create sub-folders if they don't exist for each language and move the files
+            for language, files in groups.items():
+                self.create_new_directory(language)
+
+                # Getting sub-folder path
+                subfolder = os.path.join(self.current_directory, self.files_directory, language)
+
+                # Move the files to the sub-folders
+                for file in files:
+                    source = os.path.join(self.files_directory, file)
+                    destination = os.path.join(subfolder, file)
+                    self.move_file_to_specific_location(source, destination)
+
+            groups = {}  # Reset the groups dictionary for the next patch
+
+        print("Files have been grouped into sub-folders based on language.")
+
+
+categorize_files = CategorizeLangFilesHelper()
+categorize_files.get_files_dir()
+categorize_files.grouping_files()
